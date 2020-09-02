f8bf73bebd1a7fe47eeb6850ee58ec0150eca350
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2020-09-02 23:21:11 +0530

    Update setup.sh

--- deploy/kubernetes/.travis/setup.sh
+++ deploy/kubernetes/.travis/setup.sh
@@ -11,7 +11,7 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$TRAV
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/$TRAVIS_MINIKUBE_VERSION/minikube-linux-arm64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 mkdir -p $HOME/.kube $HOME/.minikube
 touch $HOME/.kube/config
-sudo minikube start --vm-driver=none --kubernetes-version=$TRAVIS_K8S_VERSION
+sudo minikube start --vm-driver=none --kubernetes-version=$TRAVIS_K8S_VERSION --logtostderr
 sudo minikube update-context
 JSONPATH='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'; \
         until kubectl get nodes -o jsonpath="$JSONPATH" 2>&1 | grep -q "Ready=True"; do sleep 1; done
